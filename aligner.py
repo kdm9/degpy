a51afd2ae22b6a0cfc4662ee43457582e8d4290d
Kevin Murray <k.d.murray.91@gmail.com>
2012-11-29 20:56:55 +1100

    minor changes to aligner.py

--- aligner.py
+++ aligner.py
@@ -1,16 +1,26 @@
-from subprocess import POpen
+from subprocess import Popen, PIPE
+import subprocess
+from degpy import exe_path, ExeNotInstalled
 
 
 class Aligner(object):
-
-    def __init__(self):
-        pass
+    
+    def __init__(self, exes):
+        self.exepaths = []
+        for exe in exes:
+            exepath = exe_path(exe)
+            if exepath is None:
+                raise RuntimeError("Not installed: %s" % exe)
+            self.exepaths.append(exepath)
 
 
 class BWA(Aligner):
-
+    """
+    """
+    exes = ["bwa", "samtools"]  # 
     def __init__(self):
-        super(BWA, self).__init__()
+        super(BWA, self).__init__(exes)
 
     def run(self):
-        POpen()
+        pass
+
